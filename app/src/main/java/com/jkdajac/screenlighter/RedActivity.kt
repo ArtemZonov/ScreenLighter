775e0bd3f98f1f3e42b672b15cdcd94d1cde6913
ARTEM <jkda@ukr.net>
2021-07-02 20:31:34 +0300

    added RedActivity.kt

---
+++ app/src/main/java/com/jkdajac/screenlighter/RedActivity.kt
@@ -0,0 +1,12 @@
+package com.jkdajac.screenlighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_red)
+
+    }
+}
